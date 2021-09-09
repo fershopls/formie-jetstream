c07d7635fb945324ff59f1e5cd9f9a02350a049a
Fercho Posting <shaaf4ever@gmail.com>
2021-09-08 22:19:00 -0500

    Images

---
+++ database/migrations/2021_09_09_031734_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('url');
+        });
+
+        Schema::create('imageables', function (Blueprint $table) {
+            $table->unsignedBigInteger('image_id');
+            $table->unsignedBigInteger('imageable_id');
+            $table->string('imageable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('imageables');
+    }
+}
