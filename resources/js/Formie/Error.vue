ce4dea367668389c060ac25311ed28f09950b5d5
Fercho Posting <shaaf4ever@gmail.com>
2021-09-08 20:22:52 -0500

    Error message

--- resources/js/Formie/Error.vue
+++ resources/js/Formie/Error.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="text-red-600 mt-2 inline-block px-2 text-center border border-red-400 rounded">
-    <i class="fas fa-times"></i>
-    <slot />
+  <div class="flex">
+    <div class="text-red-600 mt-2 px-2 text-center border border-red-400 rounded flex items-center">
+      <div class="text-lg mr-2">
+        <i class="fas fa-exclamation-triangle"></i>
+      </div>
+      <slot />
+    </div>
   </div>
 </template>
 
