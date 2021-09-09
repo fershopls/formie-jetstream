7e182578e54decc33826cd78b2686b7acd436166
Fercho Posting <shaaf4ever@gmail.com>
2021-09-08 22:31:31 -0500

    Save images

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -29,6 +31,22 @@ class ProductController extends Controller
             'category_id' => 'required|integer',
         ]);
 
-        dd($request->all());
+        $storedUrls = collect($request->images)
+            ->map(function ($file) {
+                $path = $file->store('products');
+                return Storage::url($path);
+            });
+
+        $product = Product::create($validated);
+
+        $records = $storedUrls->map(function ($url) {
+            return ['url' => $url];
+        });
+
+        $product->images()->createMany(
+            $records->toArray()
+        );
+
+        return $product;
     }
 }
