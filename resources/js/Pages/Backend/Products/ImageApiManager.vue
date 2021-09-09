9e3ef34280d203027344966262a1e414fbae23b5
Fercho Posting <shaaf4ever@gmail.com>
2021-09-08 23:35:01 -0500

    Style

--- resources/js/Pages/Backend/Products/ImageApiManager.vue
+++ resources/js/Pages/Backend/Products/ImageApiManager.vue
@@ -1,28 +1,32 @@
 <template>
   <div
     v-if="context.model && context.model.id"
-    class="flex gap-4"
+    class="bg-gray-100 rounded-lg py-4"
   >
-    <div
-      v-for="image in context.model.images"
-      :key="image.id"
-    >
-      <a
-        :href="image.url"
-        target="_blank"
-      >
-        <div class="rounded-md bg-gray-800 overflow-hidden h-24 w-24 flex items-center justify-center">
-          <img
-            :src="image.url"
-            alt=""
-          >
-        </div>
-      </a>
+    <div class="text-xs uppercase text-center tracking-widest font-bold text-gray-400">Imágenes</div>
+    <div class="mt-3 flex gap-4 justify-center flex-wrap">
       <div
-        class="mt-2 text-center underline text-red-600 cursor-pointer"
-        @click="onDelete(context.model.id, image.id)"
+        v-for="image in context.model.images"
+        :key="image.id"
+        class="p-2 bg-white shadow-sm rounded"
       >
-        Eliminar
+        <a
+          :href="image.url"
+          target="_blank"
+        >
+          <div class="rounded-md bg-gray-800 overflow-hidden h-24 w-24 flex items-center justify-center">
+            <img
+              :src="image.url"
+              alt=""
+            >
+          </div>
+        </a>
+        <div
+          class="mt-2 tracking-wide text-center underline text-red-600 cursor-pointer"
+          @click="onDelete(context.model.id, image.id)"
+        >
+          Eliminar
+        </div>
       </div>
     </div>
   </div>
@@ -35,6 +39,8 @@ export default {
 
   methods: {
     onDelete(product_id, image_id) {
+      if (!confirm("Estas seguro?")) return;
+
       const url = route("products.images.destroy", {
         product: product_id,
         image: image_id,
